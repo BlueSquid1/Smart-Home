6dbc7fdd3c0a1d014a1cad8e9d510bf31ba13c4f
Clinton Page <clinton@Clintons-MacBook-Pro.local>
2022-05-15 20:51:06 +0930

    made security server log error on 404

--- servers/security/src/index.ts
+++ servers/security/src/index.ts
@@ -9,21 +9,30 @@ const securityApp = new securityMgr.SecurityMgr(app);
 const securityFilePath = path.join(__dirname, 'security.json');
 const result = securityApp.initalize(securityFilePath);
 if(!result) {
-  console.log("failed to initalise. Shutting down.");
-  process.exit(1);
+    console.log("failed to initalise. Shutting down.");
+    process.exit(1);
 }
 
 app.get('/api/arm', (req, res) => {
-  securityApp.armHouse();
-  res.send('arming house');
+    securityApp.armHouse();
+    res.send('arming house');
 });
 
 app.get('/api/unarm', (req, res) => {
-  securityApp.unarmHouse();
-  res.send('unarmed house');
+    securityApp.unarmHouse();
+    res.send('unarmed house');
 });
+
+
+// Last non-error route is used to detected 404 errors
+app.use(function(req, res, next){
+    console.error("404 error when client: " + req.ip + " accessing: " + req.url);
+    res.status(404);
+    res.send('404 error');
+});
+
 
 const port = 80;
 app.listen(port, () => {
-  console.log(`Security server is up on ${port}`)
+    console.log(`Security server is up on ${port}`);
 });
